ced0f405c658cc6f1d66d51b8e9c36a0a6a31efc
rus <rustamalimidjanov@gmail.com>
2023-03-31 22:23:49 +0300

    change to navGraf(failed)

--- app/src/main/java/com/example/testvm/MainActivity.kt
+++ app/src/main/java/com/example/testvm/MainActivity.kt
@@ -5,15 +5,9 @@ import android.os.Bundle
 import com.example.testvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.container, MainFragment.newInstance())
-            .commit()
     }
 }
--- app/src/main/java/com/example/testvm/MainFragment.kt
+++ app/src/main/java/com/example/testvm/MainFragment.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.testvm.databinding.FragmentMainBinding
 
-class MainFragment: Fragment() {
+class MainFragment: Fragment(R.layout.fragment_main) {
 
     lateinit var binding: FragmentMainBinding
     lateinit var dataName: Name
@@ -16,18 +16,10 @@ class MainFragment: Fragment() {
     private val nameViewModel: FragmentNameViewModel by activityViewModels()
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        dataName = Name()
-        binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
+        dataName = Name()
 
         binding.button.setOnClickListener {
             val tvText = binding.editText.text.toString()
